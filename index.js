75a93cbc21c07d970278679aa2e88e392cb2e923
Alberto Leon <alberto.leon@icloud.com>
2023-10-19 23:14:06 -0500

    version 1.2

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // Core modules
 const path = require("path");
-const http = require("http");
 
 // Third-party modules
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const socketIo = require("socket.io");
 require("dotenv").config();
 
 // Custom modules
@@ -20,12 +18,7 @@ const app = express();
 
 // Middleware configurations
 app.use(bodyParser.json());
-app.use(
-    cors({
-        origin: "https://memory-card-game-wc98.onrender.com",
-        credentials: true,
-    })
-);
+app.use(cors());
 app.use(express.static(path.join(__dirname, "../Frontend")));
 
 // Routes
@@ -34,33 +27,11 @@ app.use("/api/pixabay", pixabayRoutes);
 app.use("/api/scores", scoreRoutes);
 app.use("/api/game", gameRoutes);
 
-// Set up HTTP server and Socket.io
-const server = http.createServer(app);
-const io = socketIo(server);
-
-io.on("connection", (socket) => {
-    console.log("New client connected");
-
-    // Example: Listen for a player's move and broadcast it
-    socket.on("player_move", (data) => {
-        // TODO: Handle the player's move, validate it, update game state, etc.
-        // ...
-
-        // Broadcast the move to the other player
-        socket.broadcast.emit("opponent_move", data);
-    });
-
-    socket.on("disconnect", () => {
-        console.log("Client disconnected");
-        // TODO: Handle player disconnection, notify other player, etc.
-    });
-});
-
 // Connect to the database
 db.connect();
 
 // Start the server and listen on the defined port
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
